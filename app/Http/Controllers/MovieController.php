781058f74fcb35087f2ecd8acd7be27bd42a7b1b
Tom Witkowski <dev.gummibeer@gmail.com>
2024-03-25 17:08:44 +0100

    reject existing recommendations

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -66,7 +66,7 @@ class MovieController
     {
         $movies = Movie::query()->findMany(
             ids: File::collect(storage_path('app/movies-recommended.json'))
-        );
+        )->reject(fn (Movie $movie) => OwnedMovie::query()->where('movie_id', $movie->id)->exists());
 
         return view('recommend', [
             'movies' => $movies,
